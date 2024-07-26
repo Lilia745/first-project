f29e17e9f9eb3fc2c463c756cfe19a0c7a553c19
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-27 00:09:25 +0400

    added cont and style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="root">
+      <div className="cont">
+        <div className="left">
+          <img src="https://colorlib.com/etc/regform/colorlib-regform-3/images/bg-heading-03.jpg"></img>
+        </div>
+        <div className="right">
 
+        </div>
+      </div>
     </div>
   );
 }
