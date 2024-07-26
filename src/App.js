3ae93b0943072f5951995d03d0e9da1bdb7c262f
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-27 00:28:50 +0400

    added inputs and style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ function App() {
           <img src="https://colorlib.com/etc/regform/colorlib-regform-3/images/bg-heading-03.jpg"></img>
         </div>
         <div className="right">
-          
+          <h2>Registration Info</h2>
+          <div className="inputDiv">
+          <input type="text" placeholder="Name" required></input>
+          <input type="date" placeholder="Bhirdate" required></input>
+          <select>
+            <option>Gender</option>
+            <option>Male</option>
+            <option>Female</option>
+            <option>Other</option>
+          </select>
+          <input type="email" placeholder="Email" required></input>
+          <input type="number" placeholder="Phone" required></input>
+          </div>
         </div>
       </div>
     </div>
