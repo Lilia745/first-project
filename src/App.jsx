a9e2ac8d6a6bbba43b7ccea8407b942c8db950b4
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-27 13:57:25 +0400

    first project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const user = {
+    name:"Anna",
+    year:"2007",
+  }
+  return (
+    <p> my name is {user.name}, I born in {user.year} year, I {2024 - user.year} old years</p>
+  );
+}
+
+export default App;
