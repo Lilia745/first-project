b63c779cb1a9639c9c243caa0cf46f150d1abe16
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-27 14:02:21 +0400

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   const user = {
     name:"Anna",
     year:"2007",
+    surname:""
   }
   return (
     <p> my name is {user.name}, I born in {user.year} year, I {2024 - user.year} old years</p>
